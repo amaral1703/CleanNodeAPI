e288f1ff2f59f39ae34898d897702006cdcea1d9
amaral1703 <gamaral987654321@gmail.com>
2025-08-11 19:02:42 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from 'express'
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
+  res.set('acces-control-allow-origin', '*')
+  res.set('acces-control-allow-methods', '*')
+  res.set('acces-control-allow-headers', '*')
+  next()
+}
