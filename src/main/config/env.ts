57481eaf6e97c1026a50d90f87f30e2b46341f68
amaral1703 <gamaral987654321@gmail.com>
2025-08-12 16:44:53 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,7 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || '',
+  port: process.env.PORT || 5050,
+  nodeEnv: 'development'
+}
+
+//   nodeEnv: process.env.NODE_ENV || 'development'
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,18 @@
-import app from './config/app'
+import { MongoHelper, MongoMemoryServer } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(80, () => console.log('server running at http://localhost:80'))
+async function startServer (): Promise<void> {
+  let mongoUrl = env.mongoUrl
+  if (!mongoUrl || env.nodeEnv === 'development') {
+    const mongoServer = await MongoMemoryServer.create()
+    mongoUrl = mongoServer.getUri()
+  }
+  await MongoHelper.connect(mongoUrl)
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => console.log(`server running at http://localhost:${env.port}`))
+}
+// MongoHelper.connect(env.mongoUrl).then(() => {
+
+// })
+
+startServer().catch(console.error)
