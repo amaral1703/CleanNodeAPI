5846d65adb34c15d4d886664d3c132e6f58f5cd5
amaral1703 <gamaral987654321@gmail.com>
2025-08-14 11:20:31 -0300

    test: ensure LogMongoRepository creates an error log on succes

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,26 @@
+import { Collection } from 'mongodb'
+import { MongoHelper, MongoMemoryServer } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+describe('log Mongo Repository', () => {
+  let errorCollection: Collection
+  let mongoServer: MongoMemoryServer
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create()
+    const mongoUri = mongoServer.getUri()
+    await MongoHelper.connect(mongoUri)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    await mongoServer.stop()
+  })
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+  test('should create an error log on succes', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
