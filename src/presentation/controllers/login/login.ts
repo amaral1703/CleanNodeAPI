727d0ea04122e82e1f72686da529453385e2d634
amaral1703 <gamaral987654321@gmail.com>
2025-08-15 14:43:54 -0300

    feat: ensure LoginController calls email validator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,7 +1,12 @@
 import { MissingParamError } from '../../errors'
 import { badRequest, serverError } from '../../helpers/http-helpers'
-import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../../protocols'
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (httpRequest.body.email == null || httpRequest.body.email === undefined) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
@@ -9,6 +14,7 @@ export class LoginController implements Controller {
     if (httpRequest.body.password == null || httpRequest.body.password === undefined) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
     }
+    this.emailValidator.isValid('email@mail.com')
     return serverError(new Error('Unhandled case'))
   }
 }
