7a225cd4cfa9d5a08d33c36f80c387bac7ab1240
amaral1703 <gamaral987654321@gmail.com>
2025-08-20 15:40:38 -0300

    test: ensure AccountMongoRepository returns an account on loadbyemail success

--- src/infra/db/mongodb/account/account-repository.spec.ts
+++ src/infra/db/mongodb/account/account-repository.spec.ts
@@ -1,7 +1,9 @@
 // import '../../../../jest-globals'
+import { Collection } from 'mongodb'
 import { MongoHelper, MongoMemoryServer } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account-repository'
 
+let accountCollection: Collection
 describe('account MongoDb', () => {
   let mongoServer: MongoMemoryServer
   beforeAll(async () => {
@@ -14,20 +16,36 @@ describe('account MongoDb', () => {
     await mongoServer.stop()
   })
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
-  test('should return an account on succes', async () => {
+
+  test('should return an account on add succes', async () => {
     const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+
+  test('should return an account on LoadByEmal succes', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
