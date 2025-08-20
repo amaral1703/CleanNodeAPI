d80a4dd465fab4a38bb1f18ade69faca7d2114b5
amaral1703 <gamaral987654321@gmail.com>
2025-08-20 18:33:24 -0300

    feat: add LoginFatory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,7 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'fb453SW46FSD-34KBE5'
 }
 
 //   nodeEnv: process.env.NODE_ENV || 'development'
