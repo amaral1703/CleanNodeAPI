01ef017c657902e656fb9a7adeb8b651cfc657e9
amaral1703 <gamaral987654321@gmail.com>
2025-08-20 16:45:32 -0300

    reafctor: rename signupRoutes with loginRoutes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper, MongoMemoryServer } from '../../infra/db/mongodb/helpers/mongo-helper'
 
-describe('signup routes', () => {
+describe('login routes', () => {
   let mongoServer: MongoMemoryServer
   beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create()
@@ -17,15 +17,17 @@ describe('signup routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Should return an account on succes', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'gabriel',
-        email: 'gabriel@gmail.com',
-        password: '1234',
-        passwordConfirmation: '1234'
-      })
-      .expect(200)
+  describe('POST /singup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'gabriel',
+          email: 'gabriel@gmail.com',
+          password: '1234',
+          passwordConfirmation: '1234'
+        })
+        .expect(200)
+    })
   })
 })
