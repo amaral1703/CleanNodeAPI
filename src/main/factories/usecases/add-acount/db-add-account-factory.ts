43e055bef46ef0b6c2d621e7cac14405391a8cd9
amaral1703 <gamaral987654321@gmail.com>
2025-08-21 19:33:09 -0300

    fix: ensure Db-Add-Account-factory use LoadAccountByEmailRepository paramether

--- src/main/factories/usecases/add-acount/db-add-account-factory.ts
+++ src/main/factories/usecases/add-acount/db-add-account-factory.ts
@@ -6,5 +6,5 @@ export const makedbAddAccount = (): AddAccount => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
-  return new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
 }
