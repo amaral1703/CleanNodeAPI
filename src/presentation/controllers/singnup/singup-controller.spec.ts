8d21059b2c943300df863922082b9db386db3f04
amaral1703 <gamaral987654321@gmail.com>
2025-08-21 10:52:01 -0300

    test: ensure SignUpController returns 500 if Athentication throws

--- src/presentation/controllers/singnup/singup-controller.spec.ts
+++ src/presentation/controllers/singnup/singup-controller.spec.ts
@@ -116,4 +116,11 @@ describe('SignUp Controller', () => {
       password: 'a_password'
     })
   })
+
+  test('should return 500 if Athentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
