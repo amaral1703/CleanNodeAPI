f7928ad9d66f9d8d91e6ae644b20bb3f90ef24d9
amaral1703 <gamaral987654321@gmail.com>
2025-08-22 21:28:05 -0300

    feat: ensure AddSurveyController calls AddSurvey with correct values

--- src/presentation/controllers/login/survey/add-survey/add-survey-controller-protocols.ts
+++ src/presentation/controllers/login/survey/add-survey/add-survey-controller-protocols.ts
@@ -1,2 +1,3 @@
 export * from '../../../../protocols'
 export * from '../../../../helpers/http/http-helpers'
+export * from '../../../../../domain/usecases/add-survey'
--- src/presentation/controllers/login/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/login/survey/add-survey/add-survey-controller.ts
@@ -1,8 +1,9 @@
-import { badRequest, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
+import { AddSurvey, badRequest, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -10,6 +11,11 @@ export class AddSurveyController implements Controller {
     if (error) {
       return badRequest(error)
     }
-    return new Promise(resolve => resolve(null as unknown as HttpResponse))
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({
+      question,
+      answers
+    })
+    return null as unknown as HttpResponse
   }
 }
