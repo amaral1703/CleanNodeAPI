f84e251ef44b06f39c39c86d8d4c1dc8b3f19ce5
amaral1703 <gamaral987654321@gmail.com>
2025-08-23 22:13:51 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,38 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper, MongoMemoryServer } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+describe('survey routes', () => {
+  let mongoServer: MongoMemoryServer
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create()
+    const mongoUri = mongoServer.getUri()
+    await MongoHelper.connect(mongoUri)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    await mongoServer.stop()
+  })
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+  describe('POST /surveys', () => {
+    test('Should return 204 on AddSurveySucces', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: "Question",
+          answers: [{
+            answer: 'answer_1',
+            image: 'http://drive.com/image_1'
+          }, {
+            answer: 'answer_2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
