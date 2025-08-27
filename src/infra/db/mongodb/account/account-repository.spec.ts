97f0ad3dd4f8677954f232b7e423c3503f270331
amaral1703 <gamaral987654321@gmail.com>
2025-08-27 12:23:41 -0300

    test: ensure AccountMongoRepository find an account on LoadByToken without role

--- src/infra/db/mongodb/account/account-repository.spec.ts
+++ src/infra/db/mongodb/account/account-repository.spec.ts
@@ -81,4 +81,21 @@ describe('account MongoDb', () => {
       }
     })
   })
+  describe('loadByToken()', () => {
+    test('should return an account on LoadByToken without role succes', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+  })
 })
