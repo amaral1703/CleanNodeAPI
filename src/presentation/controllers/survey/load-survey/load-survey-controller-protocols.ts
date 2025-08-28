629cc73420e640d585791b53a02403ec9bf7bf5a
amaral1703 <gamaral987654321@gmail.com>
2025-08-28 15:01:06 -0300

    feat: ensure LoadSurveyController calls LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-survey/load-survey-controller-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../protocols'
+export * from '../../../helpers/http/http-helpers'
+export * from '../../../../domain/usecases/load-surveys'
+export * from '../../../../domain/models/survey'
---
+++ src/presentation/controllers/survey/load-survey/load-surveys-controller.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-survey-controller-protocols'
+export class LoadSurveysController implements Controller {
+  constructor (
+    private readonly loadSurveys: LoadSurveys
+  ) {
+
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return null as unknown as HttpResponse
+  }
+}
