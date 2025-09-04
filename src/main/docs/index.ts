2c45eb0f1f0993625275960e81d1233ae4764a17
amaral1703 <gamaral987654321@gmail.com>
2025-09-04 20:35:07 -0300

    feat: add Swagger API

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'clean Node API',
+    description: 'API desenvolvida Para Estudos de Api que realiza Enquetes',
+    version: '1.0.0'
+  }
+}
