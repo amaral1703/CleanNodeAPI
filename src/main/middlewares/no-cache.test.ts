26cf0d93044f986a3ac4ab2885ee0c56c6b2e0d1
amaral1703 <gamaral987654321@gmail.com>
2025-09-04 20:54:46 -0300

    test: add NoCache Middleware

---
+++ src/main/middlewares/no-cache.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import { noCache } from './no-cache'
+import app from '@/main/config/app'
+
+describe('no-cache middleware', () => {
+  test('Should desable ache', async () => {
+    app.get('/test_no_cache', noCache, (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_no_cache')
+      .expect('cache-control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+      .expect('pragma', 'no-cache')
+      .expect('Expires', '0')
+      .expect('surrogate-control', 'no-store')
+  })
+})
